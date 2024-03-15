6fd309ed118e807b7c8eae39788a27483e94f7b7
Lucas Moore <125796088+lukuky64@users.noreply.github.com>
2024-03-15 11:42:38 +1100

    Update test_line_detection.cpp

--- src/selfie_drawing_robot/src/test_line_detection.cpp
+++ src/selfie_drawing_robot/src/test_line_detection.cpp
@@ -1,3 +1,12 @@
+// Test plan for my module:
+
+// - dynamically adjust parameters for a desired level of detail
+// - supply multiple faces for some test cases
+// - supply images of differnt lighting to see adaptive contrasting and brightness
+// - test aginst images of people with different facial features (eg; hair type, beard, skin tone, etc)
+// - how many lines are produced, how smooth are they?
+// - are the shaded regions accurate?
+
 // main.cpp
 
 #include <iostream>
